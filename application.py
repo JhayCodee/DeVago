be04c92a1b3ced58fbedef0d7d0f8d5a82978407
Luisito <you@example.com>
2021-05-29 21:15:15 +0000

    login/logout

--- application.py
+++ application.py
@@ -61,7 +61,7 @@ def register():
         return redirect("/")
 
     else:
-        return render_template("register.html")
+        return render_template("login.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -86,14 +86,14 @@ def login():
                           username=request.form.get("username"))
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if len(rows) != 1 or not check_password_hash(rows[0]["contraseña"], request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows[0]["idusuario"]
 
         # Redirect user to home page
-        #eturn redirect(url_for("index"))
+        return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
