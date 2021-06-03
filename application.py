7586849a7fd7dc6ccb0014deafbddaca76fad7e8
MelissaGenet24 <you@example.com>
2021-06-03 20:20:31 +0000

    equis.py

--- application.py
+++ application.py
@@ -1,12 +1,17 @@
 from cs50 import SQL
 from flask import Flask, redirect, render_template, request, session, flash
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from tempfile import mkdtemp
+from flask_session import Session
 from helpers import apology, login_required
 
 # Configure application
 app = Flask(__name__)
-
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///devago.db")
 
