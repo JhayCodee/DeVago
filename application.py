2e75048685927a769dd57a1568c2c41405faf63f
Luisito <you@example.com>
2021-06-04 04:09:28 +0000

    rutas

--- application.py
+++ application.py
@@ -152,29 +152,44 @@ def register():
 def login():
     """Log user in"""
 
-    # Forget any user_id
+    #olvida sesion
     session.clear()
 
-    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
 
-        # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
 
-        # Query database for username
+        elif not request.form.get("ruta"):
+            return apology("debe escoger un tipo de usuario", 403)
+
+        ruta = request.form.get("ruta")
+
+        if ruta == "Admin":
+            #Peticion de Usuario
+            rows = db.execute("SELECT * FROM user WHERE nombre = :username",
+                              username=request.form.get("username"))
+
+            # verifica que sea correcta la info
+            if len(rows) != 1 or not check_password_hash(rows[0]["contraseña"], request.form.get("password")):
+                return apology("invalid username and/or password", 403)
+
+            # Rcuerda la sesion
+            session["admin"] = rows[0]["idusuario"]
+
+
+        #Peticion de Usuario
         rows = db.execute("SELECT * FROM user WHERE nombre = :username",
                           username=request.form.get("username"))
 
-        # Ensure username exists and password is correct
+        # verifica que sea correcta la info
         if len(rows) != 1 or not check_password_hash(rows[0]["contraseña"], request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
-        # Remember which user has logged in
+        # Rcuerda la sesion
         session["user_id"] = rows[0]["idusuario"]
 
         # Redirect user to home page
